d9f2d9d49540ad686c17a7e4662a70ff0184c5fd
ShebalkovD <shebalkovdenis.wk@gmail.com>
2024-10-02 20:10:59 +0300

    Прототип с генерацией вопросов и выводом ответов

---
+++ script.js
@@ -0,0 +1,74 @@
+class Question {
+    constructor(question, correct, answer1, answer2, answer3, answer4){
+        this.question = question,
+        this.correct = correct, 
+        this.answers = [correct, answer1, answer2, answer3, answer4]
+    }
+}
+
+// Генерация случайного целого числа от 0 до максимального
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}  
+
+// Генерация нового вопроса
+function addQuestion() {
+    const a = getRandomInt(100);
+    const b = getRandomInt(100);
+
+    const question = new Question(
+        `${a}+${b}`,
+        a+b,
+        getRandomInt(100),
+        getRandomInt(100),
+        getRandomInt(100),
+        getRandomInt(100)
+    );
+
+    return question;
+}
+
+// Надпись с вопросом и кнопки ответов
+const label = document.querySelector('.question');
+const buttons = document.querySelectorAll('.answer');
+
+// Вывод вопроса на экран
+function showQuestion(question) {
+    label.innerHTML = question.question;
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].innerHTML = question.answers[i];
+    }
+}
+
+// Генерация и вывод первого вопроса
+let current = addQuestion();
+showQuestion(current);
+
+// Счетчик верных ответов
+let score = 0;
+
+// Обработка нажатия на кнопку ответа
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        // При верном ответе увеличивается счетчик
+        if (Number(button.innerHTML) === current.correct) {
+            score++ ;
+            button.classList.add('correct');
+            setTimeout(() => {
+                button.classList.remove('correct');
+            }, 200);
+        }
+        else {
+            button.classList.add('wrong');
+            setTimeout(() => {
+                button.classList.remove('wrong');
+            }, 200);
+        }
+
+        // Генерация и вывод нового вопроса
+        current = addQuestion();
+        showQuestion(current);
+    })
+})
